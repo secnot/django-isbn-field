39835adba5603951a22ca5d1cfadb9e9019c28b8
Patrick R <patrick.rein@gmail.com>
2017-07-31 17:25:47 +0200

    Adds the logic for ignoring separators and adds the corresponding tests

--- isbn_field/tests.py
+++ isbn_field/tests.py
@@ -1,31 +1,47 @@
 from django.core.exceptions import ValidationError
 from isbn_field.validators import ISBNValidator
-from django.test import SimpleTestCase
 
+from django.test import TestCase
+from django.test.utils import override_settings
 
-class ISBNValidatorTest(SimpleTestCase):
+class ISBNValidatorTest(TestCase):
 
-    def test_validation(self):
-        # Short
+    def test_ISBN_too_short(self):
         with self.assertRaises(ValidationError):
             ISBNValidator('111')
 
-        # Long
+        with self.assertRaises(ValidationError):
+            ISBNValidator('')
+
+    def test_ISBN_too_long(self):
         with self.assertRaises(ValidationError):
             ISBNValidator('12345678901234')
 
-        # ISBN w Error
+    def test_ISBN_with_illegal_structure(self):
         with self.assertRaises(ValidationError):
             ISBNValidator('0765348275')
 
+    def test_dashes_as_valid_separators(self):
+        assert(ISBNValidator('0-765-348-276'))
+        assert(ISBNValidator('0-7-6-5-3-4-8-2-7-6'))
+
+    def test_spaces_as_valid_separators(self):
+        assert (ISBNValidator('0 765 348 276'))
+        assert (ISBNValidator('0 7 6 5 3 4 8 2 7 6'))
+
+    def test_space_and_dash_as_valid_separators(self):
+        assert (ISBNValidator('0 765-348 276'))
+        assert (ISBNValidator('0 7 6 5-3 4 8 2 7 6'))
+
+    def test_valid_ISBNs(self):
         # Valid ISBN10
-        ISBNValidator('0765348276')
+        assert(ISBNValidator('0765348276'))
 
         # Valid ISBN13
-        ISBNValidator('9780765348272')
+        assert(ISBNValidator('9780765348272'))
 
         # Valid ISBN10
-        ISBNValidator('832070801X')
+        assert(ISBNValidator('832070801X'))
 
         # Valid ISBN13
-        ISBNValidator('9788320708011')
+        assert(ISBNValidator('9788320708011'))
