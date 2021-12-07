1397fb8221cd9db195c08d04b858509a9966245a
secnot <secnot@secnot.com>
2021-12-07 22:09:01 +0100

    tests: Add id field to test models to remove Warning message

--- tests/models.py
+++ tests/models.py
@@ -2,19 +2,19 @@ from django.db import models
 from isbn_field import ISBNField
 
 class CleanISBNModel(models.Model):
-
+    id = models.BigAutoField(primary_key=True)
     isbn = ISBNField() # clean_isbn=True (default)
 
 
 class DirtyISBNModel(models.Model):
-    
+    id = models.BigAutoField(primary_key=True)
     isbn = ISBNField(clean_isbn=False)
 
 
 class BlankISBNModel(models.Model):
-
+    id = models.BigAutoField(primary_key=True)
     isbn = ISBNField(clean_isbn=True, blank=True)
 
 class BlankNullISBNModel(models.Model):
-
+    id = models.BigAutoField(primary_key=True)
     isbn = ISBNField(clean_isbn=True, blank=True, null=True)
